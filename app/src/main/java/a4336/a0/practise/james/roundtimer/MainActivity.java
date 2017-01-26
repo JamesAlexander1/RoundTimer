19af289178394017fda94dffca4ceedf5550c963
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2017-01-26 12:06:35 +1100

    added basic functionality to MainActivity xml, started View code, should add to Presenter

--- app/src/main/java/a4336/a0/practise/james/roundtimer/MainActivity.java
+++ app/src/main/java/a4336/a0/practise/james/roundtimer/MainActivity.java
@@ -2,6 +2,10 @@ package a4336.a0.practise.james.roundtimer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TableLayout;
+import android.widget.ToggleButton;
 
 import a4336.a0.practise.james.roundtimer.Model.TimerModel;
 import a4336.a0.practise.james.roundtimer.Presenter.MainPresenter;
@@ -19,11 +23,33 @@ public class MainActivity extends AppCompatActivity {
 
 
         Initialise();
+        InitialiseButtonsAndViews();
     }
 
     private void Initialise(){
 
         presenter = new MainPresenter(new TimerModel());
+    }
+    private void InitialiseButtonsAndViews(){
+
+        final TableLayout hiddenTable = (TableLayout) findViewById(R.id.Hidden_Table);
+        hiddenTable.setVisibility(View.INVISIBLE);
+
+        ToggleButton toggleBreakRoundsButton = (ToggleButton) findViewById(R.id.toggleBreakButton);
+
+        toggleBreakRoundsButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+
+                if(isChecked){
+                    hiddenTable.setVisibility(View.VISIBLE);
+                }else{
+                    hiddenTable.setVisibility(View.INVISIBLE);
+                }
+            }
+
+
+
+        });
     }
     @Override
     protected void onStart() {
